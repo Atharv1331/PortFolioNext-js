b4fd172d5c3f68578c84024391bc54a143083fbe
Atharv Kute <atharv.kute@maximusinfoware.in>
2024-10-05 12:35:54 +0530

    Init

---
+++ data/index.ts
@@ -0,0 +1,268 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Skills", link: "#skills" },
+    { name: "Experience", link: "#Experience"},
+    { name: "Approach", link: "#approach"},
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "Extensive experience in Python programming, with expertise in Django and Flask frameworks for web development, along with a strong background as a data scientist and data analyst, specializing in machine learning models and algorithms.",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently working as a AI/ML Developer",
+      description: "",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to contact?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Predictive Analysis For Machine Maintainance",
+      des: "Predictive Machine Maintenance using data analytics and machine learning techniques like Regression Analysis,Classification Alogorithms,Neural Networks & Data Visualization.",
+      img: "/vs1.jpg",
+      iconLists: ["/python-5.svg", "/power-bi-icon.svg", "/logo-javascript.svg", "/three.svg", "/fm.svg"],
+      link: "",
+    },
+    {
+      id: 2,
+      title: "Bank Transaction Forecasting",
+      des: "Bank Transaction Success, Unsuccessful, and Hanging Transaction Future Prediction Using Time Series Forecasting (ARIMA, SARIMA) involves employing advanced statistical techniques.",
+      img: "/tsf.jpg",
+      iconLists: ["/python-5.svg", "/power-bi-icon.svg", "/logo-javascript.svg", "/stream.svg", "/c.svg"],
+      link: "",
+    },
+    {
+      id: 3,
+      title: "FRMS - Fraud Risk Management System",
+      des: "FRMS - Fraud Risk Management System is a comprehensive solution designed to detect, assess, and mitigate fraud risks in real-time, utilizing advanced analytics, machine learning algorithms.",
+      img: "/FRMS.jpg",
+      iconLists: ["/django.svg", "/python-5.svg", "/logo-javascript.svg", "/power-bi-icon.svg", "/html-1.svg"],
+      link: "",
+    },
+    {
+      id: 4,
+      title: "Chatbot using LLM",
+      des: "Chatbot using LLM (Large Language Model) leverages advanced natural language processing techniques to provide human-like interactions, offering personalized responses.",
+      img: "/Chatbot.jpg",
+      iconLists: ["/python-5.svg", "/html-1.svg", "/css-3.svg", "/logo-javascript.svg", "/three.svg"],
+      link: "",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Extensive experience in Python programming, with expertise in ML models and algorithms.",
+      name: "Python Development",
+      title: "",
+    },
+    {
+      quote:
+        "Skilled in designing and deploying robust APIs to facilitate seamless integration between systems.",
+      name: "API Design and Implementation",
+      title: "",
+    },
+    {
+      quote:
+        "Proficient in developing machine learning models and deep learning architectures for various applications.",
+      name: "Machine Learning & Deep Learning",
+      title: "",
+    },
+    {
+      quote:
+        "Experienced in implementing NLP techniques for text analysis and processing. Also experienced in leveraging the power of LLM (Large Language Models).",
+      name: "Natural Language Processing (NLP)",
+      title: "",
+    },
+    {
+      quote:
+        "Strong knowledge of Python libraries such as Pandas, NumPy, Matplotlib, and more.",
+      name: "Python Libraries",
+      title: "",
+    },
+    {
+      quote:
+        "Skilled in ORM tools like SQL Alchemy and Django ORM for database management.",
+      name: "Object-Relational Mapping (ORM)",
+      title: "",
+    },
+    {
+      quote:
+        "Proficient in using Microsoft Power BI for creating interactive and insightful data visualizations.",
+      name: "Microsoft Power BI",
+      title: "",
+    },
+    {
+      quote:
+        "Experienced in SQL for database querying and management.",
+      name: "SQL",
+      title: "",
+    },
+    {
+      quote:
+        "Proficient in HTML, CSS, and JavaScript for developing responsive and user-friendly web interfaces.",
+      name: "Front-End Development",
+      title: "",
+    },
+    {
+      quote:
+        "Strong analytical and problem-solving skills with a proven track record of developing innovative solutions.",
+      name: "Problem-Solving",
+      title: "",
+    },
+    {
+      quote:
+        "Adept at working independently and collaboratively in team environments.",
+      name: "Effective Collaboration",
+      title: "",
+    },
+    {
+      quote:
+        "Extensive experience in web development using Django and Flask frameworks.",
+      name: "Django & Flask Framework",
+      title: "",
+    },
+    {
+      quote:
+        "Expert in building responsive and scalable web applications.",
+      name: "Web Development",
+      title: "",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "AI/ML Developer",
+      desc: "Designed and deployed machine learning models to solve complex business problems, incorporating cutting-edge algorithms and data analysis techniques.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Web Developer (Django/Flask Framework)",
+      desc: "Developed dynamic web applications and APIs using Django and Flask, ensuring seamless backend integration and responsive front-end design.",
+      className: "md:col-span-2",
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Data Analyst",
+      desc: "Performed data cleaning, analysis, and visualization using tools like Python and SQL, delivering actionable insights to drive business growth.",
+      className: "md:col-span-2",
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Python Developer",
+      desc: "Engineered efficient, scalable solutions across multiple domains using Python, from automation scripts to complex data processing workflows.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+    },
+  ];
